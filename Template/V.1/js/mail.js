edd1493d00aad7d96bab6a3c59f9d69b0df74926
David Mangas Hernandez <148961050+davidmh-7@users.noreply.github.com>
2024-06-17 12:51:25 +0200

    Update mail.js
    Actualizado mail

--- Template/V.1/js/mail.js
+++ Template/V.1/js/mail.js
@@ -1,13 +1,19 @@
+//Luego hay que quitar el api key cuando este implementado en el PHP
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '31e014dbf2msh7dfbd3e9312cda5p158aadjsn49c0d74aef02',
+        'X-RapidAPI-Host': 'virtual-number.p.rapidapi.com'
+    }
+};
 
 
-function actualizarPagina() {
-    
-}
+fetch(`https://virtual-number.p.rapidapi.com/api/v1/e-sim/country-numbers?countryId=${countryId}`, options)
+.then(response => response.json())
 
+.catch(error => {
+    console.error("Error al cargar el archivo:", error);
+});
 
-
-function actualizarPagina() {
-    
-}
 
 
